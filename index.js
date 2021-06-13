d8f849f8874c7b16dbc445d4d52859a249ba8d54
Ankeeta Sahoo <sahooankeeta@gmail.com>
2021-06-14 01:48:05 +0530

    database connection

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
 const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
@@ -7,7 +9,17 @@ app.use(express.static("./assets"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use("/", require("./routes"));
-
+dotenv.config({ path: "./config.env" });
+const DB = process.env.DATABASE.replace("<password>", process.env.PASSWORD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log("connection made to database");
+  });
 app.listen(port, function (err) {
   if (err) console.log("error in server");
   console.log(`server running on ${port}`);
